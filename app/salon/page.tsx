18cf77c7e576b0e3704533fbe17599cd242cce6c
Carlos Valencia <carlos.valencia@vibeconsulting.com.co>
2026-08-27 11:57:47 -0500

    feat: integrate game data and leaderboard into HallOfFame component

--- app/salon/page.tsx
+++ app/salon/page.tsx
@@ -1,5 +1,12 @@
 import { HallOfFame } from "@/components/HallOfFame";
+import { getGames, getLeaderboard, type ScoreRow } from "@/lib/games-data";
 
-export default function SalonPage() {
-  return <HallOfFame />;
+export default async function SalonPage() {
+  const games = await getGames();
+  const leaderboardEntries = await Promise.all(
+    games.map(async (g) => [g.id, await getLeaderboard(g.id, 12)] as [string, ScoreRow[]])
+  );
+  const leaderboards = Object.fromEntries(leaderboardEntries);
+
+  return <HallOfFame games={games} leaderboards={leaderboards} />;
 }
